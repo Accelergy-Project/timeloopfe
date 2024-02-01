caeec18456ae4d72823285c63d076aa7689d7efc
tanner-andrulis <81568882+tanner-andrulis@users.noreply.github.com>
2024-02-01 11:06:55 -0500

    Removed many Sparseloop required actions

--- timeloopfe/v4/processors/required_actions.py
+++ timeloopfe/v4/processors/required_actions.py
@@ -15,31 +15,36 @@ class RequiredActionsProcessor(Processor):
     - read
     - write
     - update
-    Storage if metadata attributes are present:
-    - metadata_read
-    - metadata_write
-    - metadata_update
-    Storage if sparse optimization is enabled:
-    - gated_read
-    - gated_write
-    - gated_update
-    - skipped_read
-    - skipped_write
-    - skipped_update
-    - decompression_count
-    - compression_count
-    Storage if sparse and metadata are enabled:
-    - gated_metadata_read
-    - skipped_metadata_read
-    - gated_metadata_write
-    - skipped_metadata_write
-    - gated_metadata_write
-    - skipped_metadata_write
-    Compute:
-    - compute
-    Compute if sparse optimization is enabled:
-    - gated_compute
-    - skipped_compute
+    # - metadata_update
+
+    DEPRECATED. NOW ALL OF THE FOLLOWIGN ARE NOT REQUIRED.
+    Deprecated because: Skipped/gated actions can just be no action
+    decompression_count/compression_count is not supported in many of the
+    plug-ins that Sparseloop uses
+    # Storage if metadata attributes are present:
+    # - metadata_read
+    # - metadata_write
+    # Storage if sparse optimization is enabled:
+    # - gated_read
+    # - gated_write
+    # - gated_update
+    # - skipped_read
+    # - skipped_write
+    # - skipped_update
+    # - decompression_count
+    # - compression_count
+    # Storage if sparse and metadata are enabled:
+    # - gated_metadata_read
+    # - skipped_metadata_read
+    # - gated_metadata_write
+    # - skipped_metadata_write
+    # - gated_metadata_write
+    # - skipped_metadata_write
+    # Compute:
+    # - compute
+    # Compute if sparse optimization is enabled:
+    # - gated_compute
+    # - skipped_compute
     """
 
     def __init__(self, *args, **kwargs):
@@ -53,16 +58,16 @@ class RequiredActionsProcessor(Processor):
         sparse_action = not Node.isempty_recursive(sparse_opts.action_optimization)
         read_write_update = [""]
         required_actions = ["leak"]
-        if has_metadata:
-            read_write_update.append("metadata_")
-        if sparse_action:
-            read_write_update.append("gated_")
-            read_write_update.append("skipped_")
-        if sparse_action and has_metadata:
-            read_write_update.append("gated_metadata_")
-            read_write_update.append("skipped_metadata_")
-        if sparse_rep:
-            required_actions += ["decompression_count", "compression_count"]
+        # if has_metadata:
+        #     read_write_update.append("metadata_")
+        # if sparse_action:
+        #     read_write_update.append("gated_")
+        #     read_write_update.append("skipped_")
+        # if sparse_action and has_metadata:
+        #     read_write_update.append("gated_metadata_")
+        #     read_write_update.append("skipped_metadata_")
+        # if sparse_rep:
+        #     required_actions += ["decompression_count", "compression_count"]
         for r in read_write_update:
             for a in ["read", "write", "update"]:
                 required_actions.append(r + a)
@@ -72,8 +77,8 @@ class RequiredActionsProcessor(Processor):
         sparse_opts = elem.sparse_optimizations
         sparse_action = not Node.isempty(sparse_opts.action_optimization)
         required_actions = ["compute"]
-        if sparse_action:
-            required_actions += ["gated_compute", "skipped_compute"]
+        # if sparse_action:
+        #     required_actions += ["gated_compute", "skipped_compute"]
         elem.required_actions = list(set(required_actions + elem.required_actions))
 
     def process(self, spec: Specification):

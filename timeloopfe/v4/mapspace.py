fcc1ee50e29d538d961b8a9c6520c5e49ca13d53
tanner-andrulis <andrulis@mit.edu>
2024-04-26 14:37:42 -0400

    Default is Uber

--- timeloopfe/v4/mapspace.py
+++ timeloopfe/v4/mapspace.py
@@ -16,7 +16,7 @@ class Mapspace(DictNode):
     def declare_attrs(cls, *args, **kwargs):
         super().declare_attrs(*args, **kwargs)
         super().add_attr("version", default="0.4", callfunc=assert_version)
-        super().add_attr("template", str, "ruby")
+        super().add_attr("template", str, "uber")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
